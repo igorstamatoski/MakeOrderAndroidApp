67896d7a0eeab7602c05e220dd2098fc5cd63e6e
Igor Stamatoski <igorstamatoski@hotmail.com>
2020-04-29 01:41:23 +0200

    Implementation of Main Activity, Sign In and Sign Up

--- app/src/main/java/com/example/makeorderandroidapp/MainActivity.java
+++ app/src/main/java/com/example/makeorderandroidapp/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.makeorderandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.MailTo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, getBtnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        getBtnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/Nabila.ttf");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+
+            }
+        });
+
+        getBtnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/makeorderandroidapp/SignIn.java
@@ -0,0 +1,84 @@
+package com.example.makeorderandroidapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.makeorderandroidapp.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignIn extends AppCompatActivity {
+
+    private int INTERNET_PERMISSION_CODE = 1;
+    EditText edtPhone, edtPassword;
+    Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText) findViewById(R.id.edtPhone);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        //Init Firebase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Please wait...");
+                mDialog.show();
+
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        //Check if user not exist in database
+                        if(dataSnapshot.child(edtPhone.getText().toString()).exists())
+                        {
+                        mDialog.dismiss();
+
+                        //Get User Information
+                        User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+
+                        if(user.getPassword().equals(edtPassword.getText().toString())){
+                            Toast.makeText(SignIn.this,"Sign in successfully!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(SignIn.this,"Wrong password, please try again!", Toast.LENGTH_SHORT).show();
+                        }
+                        } else {
+                            mDialog.dismiss();
+                            Toast.makeText(SignIn.this,"User not found, please Sign Up!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
