4eaf8aaa242b6f21ae9c1396ec4f2f7b348cccbe
Igor Stamatoski <igorstamatoski@hotmail.com>
2020-05-10 23:01:46 +0200

    Search Items Implementation

--- app/src/main/java/com/example/makeorderandroidapp/ItemsList.java
+++ app/src/main/java/com/example/makeorderandroidapp/ItemsList.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,9 +15,16 @@ import com.example.makeorderandroidapp.Interface.ItemClickListener;
 import com.example.makeorderandroidapp.Model.ShopItem;
 import com.example.makeorderandroidapp.ViewHolder.ItemsViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemsList extends AppCompatActivity {
 
@@ -27,6 +36,11 @@ public class ItemsList extends AppCompatActivity {
     FirebaseRecyclerAdapter<ShopItem, ItemsViewHolder> adapter;
 
     String categoryId = "";
+
+    //Search Functionality
+    FirebaseRecyclerAdapter<ShopItem, ItemsViewHolder> searchAdapter;
+    List<String> suggestList = new ArrayList<>();
+    MaterialSearchBar materialSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +67,109 @@ public class ItemsList extends AppCompatActivity {
         {
             loadItemsList(categoryId);
         }
+
+        //Search
+        materialSearchBar = (MaterialSearchBar)findViewById(R.id.searchBar);
+        materialSearchBar.setHint("Enter your item");
+        loadSuggest();
+
+        materialSearchBar.setLastSuggestions(suggestList);
+        materialSearchBar.setCardViewElevation(10);
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                List<String> suggest = new ArrayList<String>();
+                for (String search: suggestList)
+                {
+                    if(search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
+                    {
+                        suggest.add(search);
+                    }
+                    materialSearchBar.setLastSuggestions(suggest);
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+
+                if(!enabled)
+                    recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+
+                startSearch(text);
+
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+        
+    }
+
+    private void startSearch(CharSequence text) {
+
+        searchAdapter = new FirebaseRecyclerAdapter<ShopItem, ItemsViewHolder>(
+                ShopItem.class,
+                R.layout.shop_item,
+                ItemsViewHolder.class,
+                itemList.orderByChild("Name").equalTo(text.toString())
+        ) {
+            @Override
+            protected void populateViewHolder(ItemsViewHolder itemsViewHolder, ShopItem shopItem, int i) {
+                itemsViewHolder.txtItemName.setText(shopItem.getName());
+                Picasso.with(getBaseContext()).load(shopItem.getImage())
+                        .into(itemsViewHolder.imageView);
+
+                final ShopItem local = shopItem;
+                itemsViewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        //Start new Activity
+                        Intent itemDetail = new Intent(ItemsList.this, ItemDetails.class);
+                        itemDetail.putExtra("ItemId",searchAdapter.getRef(position).getKey());
+                        startActivity(itemDetail);
+                    }
+                });
+            }
+        };
+
+        recyclerView.setAdapter(searchAdapter);
+    }
+
+    private void loadSuggest() {
+        itemList.orderByChild("CatId").equalTo(categoryId)
+        .addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot postSnapshot:dataSnapshot.getChildren()){
+                    ShopItem item = postSnapshot.getValue(ShopItem.class);
+                    suggestList.add(item.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void loadItemsList(String categoryId) {
